f70a8694adc04c0506b4b4e565cad9925009a140
W. Santos <w.santos.gg@gmail.com>
2023-04-20 23:00:25 +0800

    adjusted permissions

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from discord import Member, VoiceState, VoiceChannel, TextChannel, Guild, CategoryChannel
+from discord import Member, VoiceState, VoiceChannel, TextChannel, Guild, CategoryChannel, PermissionOverwrite
 
 import asyncio
 
@@ -14,6 +14,7 @@ class GuildSetup:
 class Room:
     owner: Member
     main_vc: VoiceChannel
+    waiting_room: VoiceChannel
     no_mics: TextChannel
     guild: Guild
     active: True
@@ -26,9 +27,27 @@ class Room:
         self.guild = gs.guild
 
     async def create_channels(self, gs: GuildSetup):
-        vc = await gs.guild.create_voice_channel(f'{self.owner.name}\'s Room', category=gs.generator_category)
+        # main_vc_permissions = PermissionOverwrite(connect=False)
+        # no_mics_permissions = Permissions()
+        #
+        # wr_vc_permissions = Permissions(connect=True)
 
-        channel = await gs.guild.create_text_channel(f'{self.owner.name}\'s Room (no-mics)', category=gs.generator_category, position=vc.position-1)
+        main_vc_overwrites = {
+            gs.guild.default_role: PermissionOverwrite(connect=False),
+            self.owner: PermissionOverwrite(connect=True)
+        }
+
+        wr_vc_overwrites = {
+            gs.guild.default_role: PermissionOverwrite(connect=True)
+        }
+
+        no_mics_overwrites = {
+            gs.guild.default_role: PermissionOverwrite(view_channel=False),
+            self.owner: PermissionOverwrite(connect=True)
+        }
+
+        vc = await gs.guild.create_voice_channel(f'{self.owner.name}\'s Room', category=gs.generator_category, overwrites=main_vc_overwrites)
+        channel = await gs.guild.create_text_channel(f'{self.owner.name}\'s Room (no-mics)', category=gs.generator_category, position=vc.position-1, overwrites=no_mics_overwrites)
 
         self.main_vc = vc
         self.no_mics = channel
