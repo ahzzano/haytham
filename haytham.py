2c88dec78d6833adf034b9cdebd34a6f222c560f
W. Santos <w.santos.gg@gmail.com>
2023-04-21 13:58:06 +0800

    cleaner imports

--- haytham.py
+++ haytham.py
@@ -1,12 +1,10 @@
 
 import discord
-from discord import Member, VoiceState, Embed
+from discord import Embed
 from discord.ext import commands
 
 from classes import Room, GuildSetup
 from views import WaitingRoomView
-
-import asyncio
 
 import json
 import os
